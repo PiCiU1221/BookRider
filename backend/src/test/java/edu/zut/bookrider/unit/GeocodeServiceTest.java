54abdebf394338330074c23439ed8bde387f8741
PiCiU1221 <96005164+PiCiU1221@users.noreply.github.com>
2024-12-01 22:23:42 +0100

    Fix small issues missed in previous commits

--- backend/src/test/java/edu/zut/bookrider/unit/GeocodeServiceTest.java
+++ backend/src/test/java/edu/zut/bookrider/unit/GeocodeServiceTest.java
@@ -2,7 +2,6 @@ package edu.zut.bookrider.unit;
 
 import edu.zut.bookrider.dto.CoordinateDTO;
 import edu.zut.bookrider.exception.AddressNotFoundException;
-import edu.zut.bookrider.exception.InvalidCoordinatesException;
 import edu.zut.bookrider.service.GeocodeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
