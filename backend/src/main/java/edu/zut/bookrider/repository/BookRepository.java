8e83a61a0985e8091817fc1b903938f1cfbda632
Vykpish Dzmitry <vd53816@zut.edu.pl>
2024-12-10 20:48:01 +0100

    Added book filtering method by library, category, author, and release year

---
+++ backend/src/main/java/edu/zut/bookrider/repository/BookRepository.java
@@ -0,0 +1,23 @@
+package edu.zut.bookrider.repository;
+
+import edu.zut.bookrider.model.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface BookRepository extends JpaRepository<Book, Integer> {
+    @Query("SELECT b FROM Book b " +
+           "LEFT JOIN b.libraries l " +
+           "LEFT JOIN b.authors a " +
+           "WHERE (:libraryId IS NULL OR l.id = :libraryId) " +
+           "AND (:categoryId IS NULL OR b.category.id = :categoryId) " +
+           "AND (:authorName IS NULL OR a.name LIKE %:authorName%) " +
+           "AND (:releaseDate IS NULL OR b.releaseYear = :releaseYear)")
+    Page<Book> findAllByFilters(@Param("libraryId") Integer libraryId,
+                                 @Param("categoryId") Integer categoryId,
+                                 @Param("authorName") String authorName,
+                                 @Param("releaseYear") String releaseYear,
+                                 Pageable pageable);
+}
