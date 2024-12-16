d7d9c9ac290da1d3404dacd0ade3bb07a25fecfe
Vykpish Dzmitry <vd53816@zut.edu.pl>
2024-12-16 06:07:40 +0100

    Update PublisherRepository with additional functionality

--- backend/src/main/java/edu/zut/bookrider/repository/PublisherRepository.java
+++ backend/src/main/java/edu/zut/bookrider/repository/PublisherRepository.java
@@ -4,4 +4,5 @@ import edu.zut.bookrider.model.Publisher;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PublisherRepository extends JpaRepository<Publisher, Integer> {
+    boolean existsByName(String name);
 }
