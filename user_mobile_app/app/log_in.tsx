847dba3034a4d7213cff14dfe40b14df30e0e328
Miads002 <kczarniak2@gmail.com>
2024-12-17 22:32:44 +0100

    done prototype log in page

--- user_mobile_app/app/log_in.tsx
+++ user_mobile_app/app/log_in.tsx
@@ -1,26 +1,48 @@
-import { StyleSheet,Text, View, TextInput} from "react-native";
+import {StyleSheet, Text, View, TextInput, Button, Pressable} from "react-native";
 import {Link} from 'expo-router';
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 
 export default function log_in() {
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
-            <Text>login?</Text>
-            <Text> Username/e-mail</Text>
-            <TextInput
-            style={styles.input}
+        <SafeAreaProvider>
+            <SafeAreaView
+                style={{
+                    height: 700,
+                    width: 350,
+                    flexDirection: 'row',
+                    alignItems: "center",
+                    flex: 1,
+                    justifyContent:"center"
+                }}>
+                <View
+                style={{
+                    //flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    borderWidth: 1,
+                    borderColor: "black"
+                }}
+                >
+                    <Text>login?</Text>
+                    {/*<Text> Username/e-mail</Text>*/}
+                    <TextInput
+                    style={styles.input}
+                    placeholder="Username/e-mail"
+                    >
 
-            >
+                    </TextInput>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Password"
+                    >
 
-            </TextInput>
-            <Link href="/register" style={{color:'blue'}}>Resgister now!</Link>
-        </View>
+                    </TextInput>
+                    <Button title="Log in!" color="#009933" />
+                        <Link href="/book_search" style={{color:'blue'}}>Temporary link to book search page </Link>
+                    <Link href="/register" style={{color:'blue'}}>Resgister now!</Link>
+                </View>
+            </SafeAreaView>
+        </SafeAreaProvider>
     );
 }
 const styles = StyleSheet.create({
@@ -29,5 +51,9 @@ const styles = StyleSheet.create({
         margin: 12,
         borderWidth: 1,
         padding: 10,
+
+        backgroundColor: '#ffffff',
+        width: 195 ,
+
     },
 });
