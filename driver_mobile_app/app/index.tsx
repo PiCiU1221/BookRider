1f36ba68501623f800050de5324ab5728d8304ec
Miads002 <kczarniak2@gmail.com>
2024-12-18 00:48:57 +0100

    did setup, copied log_in and registration from user application, did small changes to links so they don't point to non-existing pages, added file for driver application page

--- driver_mobile_app/app/index.tsx
+++ driver_mobile_app/app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import {Link} from "expo-router";
 
 export default function Index() {
   return (
@@ -9,7 +10,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Driver app</Text>
+        <Link href="/log_in" style={{color:'blue'}}> Log in here</Link>
     </View>
   );
 }
