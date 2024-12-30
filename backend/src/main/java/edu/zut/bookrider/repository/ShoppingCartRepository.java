ba433573f7ac4c47617eabcb052da0b3a0b21bd3
PiCiU1221 <96005164+PiCiU1221@users.noreply.github.com>
2024-12-30 19:58:11 +0100

    Remove unused findByUserId method in shopping cart repository

--- backend/src/main/java/edu/zut/bookrider/repository/ShoppingCartRepository.java
+++ backend/src/main/java/edu/zut/bookrider/repository/ShoppingCartRepository.java
@@ -3,8 +3,5 @@ package edu.zut.bookrider.repository;
 import edu.zut.bookrider.model.ShoppingCart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Integer> {
-    Optional<ShoppingCart> findByUserId(String userId);
 }
