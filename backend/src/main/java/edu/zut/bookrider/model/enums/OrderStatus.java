27529104e6058692df184989f351b0bc8bd18bcf
PiCiU1221 <96005164+PiCiU1221@users.noreply.github.com>
2025-01-13 19:58:22 +0100

    Refactor order status enum to our needs

--- backend/src/main/java/edu/zut/bookrider/model/enums/OrderStatus.java
+++ backend/src/main/java/edu/zut/bookrider/model/enums/OrderStatus.java
@@ -3,9 +3,8 @@ package edu.zut.bookrider.model.enums;
 public enum OrderStatus {
     PENDING,
     ACCEPTED,
-    PROCESSING,
-    IN_TRANSIT,
+    DECLINED,
+    DRIVER_PICKED,
+    IN_TRANSIT_TO_CUSTOMER,
     DELIVERED,
-    CANCELED,
-    DECLINED
 }
