8bd3f1d1aebb38344481c6e6eee0421cba983ea0
PiCiU1221 <96005164+PiCiU1221@users.noreply.github.com>
2025-01-13 21:17:32 +0100

    Add code that exposes authorization header in cors settings

--- backend/src/main/java/edu/zut/bookrider/security/SecurityConfig.java
+++ backend/src/main/java/edu/zut/bookrider/security/SecurityConfig.java
@@ -42,7 +42,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
