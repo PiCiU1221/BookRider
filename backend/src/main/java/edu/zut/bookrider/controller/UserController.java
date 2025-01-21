b1f357afa374477d5afa0877d509a999efd8a3c3
Vykpish Dzmitry <vd53816@zut.edu.pl>
2025-01-21 21:06:49 +0100

    Modified UserController - change password endpoint.

--- backend/src/main/java/edu/zut/bookrider/controller/UserController.java
+++ backend/src/main/java/edu/zut/bookrider/controller/UserController.java
@@ -1,15 +1,16 @@
 package edu.zut.bookrider.controller;
 
+import edu.zut.bookrider.dto.ChangePasswordDto;
 import edu.zut.bookrider.dto.IsVerifiedResponseDto;
 import edu.zut.bookrider.dto.UserIdResponseDto;
 import edu.zut.bookrider.model.User;
 import edu.zut.bookrider.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -33,5 +34,15 @@ public class UserController {
         boolean isVerified = user.getIsVerified();
 
         return ResponseEntity.ok(new IsVerifiedResponseDto(isVerified));
+    }
+    @PreAuthorize("hasRole('librarian')")
+    @PutMapping("/{username}/change-password")
+    public ResponseEntity<?> changePassword(
+            @PathVariable String username,
+            @RequestBody @Valid ChangePasswordDto changePasswordDto,
+            Authentication authentication) {
+
+        userService.changePassword(authentication, username, changePasswordDto);
+        return ResponseEntity.noContent().build();
     }
 }
