9135239b455be86eb0d18fd43316349f02e8ace7
PiCiU1221 <96005164+PiCiU1221@users.noreply.github.com>
2025-02-19 17:02:56 +0100

    Refactor swagger config to work with any url

--- backend/src/main/java/edu/zut/bookrider/config/SwaggerConfig.java
+++ backend/src/main/java/edu/zut/bookrider/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package edu.zut.bookrider.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +12,11 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "/", description = "Default Server URL")
+        }
+)
 public class SwaggerConfig {
 
     @Bean
