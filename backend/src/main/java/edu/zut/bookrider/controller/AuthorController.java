9624a98f432d334f99edbc5fd9c3522d0365003b
PiCiU1221 <96005164+PiCiU1221@users.noreply.github.com>
2025-03-08 19:19:34 +0100

    Add create author endpoint

--- backend/src/main/java/edu/zut/bookrider/controller/AuthorController.java
+++ backend/src/main/java/edu/zut/bookrider/controller/AuthorController.java
@@ -1,15 +1,16 @@
 package edu.zut.bookrider.controller;
 
+import edu.zut.bookrider.dto.AttributeAddRequestDto;
 import edu.zut.bookrider.dto.FilterResponseDTO;
 import edu.zut.bookrider.service.AuthorService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +28,14 @@ public class AuthorController {
         List<FilterResponseDTO> authors = authorService.searchAuthors(name, pageable);
 
         return ResponseEntity.ok(authors);
+    }
+
+    @PreAuthorize("hasRole('librarian')")
+    @PostMapping
+    public ResponseEntity<FilterResponseDTO> addAuthor(
+            @RequestBody @Valid AttributeAddRequestDto attributeAddRequestDto) {
+
+        FilterResponseDTO addedAuthor = authorService.addAuthor(attributeAddRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(addedAuthor);
     }
 }
