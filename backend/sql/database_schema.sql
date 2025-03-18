1185dc06283f2705892d0d92911f6feaf756fcbf
PiCiU1221 <96005164+PiCiU1221@users.noreply.github.com>
2025-03-18 19:30:11 +0100

    Fix not unique library names

--- backend/sql/database_schema.sql
+++ backend/sql/database_schema.sql
@@ -68,7 +68,7 @@ CREATE TABLE libraries
 (
     id SERIAL PRIMARY KEY,
     address_id INT NOT NULL REFERENCES addresses(id),
-    name VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL UNIQUE,
     phone_number VARCHAR(20),
     email VARCHAR(100),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
